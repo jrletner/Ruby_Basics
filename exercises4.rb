9a0a78db713488a76d32135521163edd195fa44a
jrletner <jrletner@gmail.com>
2024-01-07 11:08:52 -0600

    Add sound attribute and print_details method to Animal class

--- exercises4.rb
+++ exercises4.rb
@@ -1,10 +1,20 @@
 class Animal
-  attr_accessor :name
+  attr_accessor :name, :sound
 
-  def initialize(name)
+  def initialize(name, sound)
     @name = name
+    @sound = sound
+  end
+
+  def print_details
+    print_details_private()
+  end
+
+  private
+  def print_details_private
+    puts "#{@name} says #{@sound}"
   end
 end
 
-animal = Animal.new("Dog")
-puts animal.name
+animal = Animal.new("Dog", "Woof!")
+animal.print_details
